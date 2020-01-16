5f877b39babf9b889f16b24f37dad88b97d52548
Aaron Vogler <Aaron.Vogler@Colorado.EDU>
2020-01-15 19:26:06 -0700

    Create user model and add users as an installed app, changing AUTH_MODEL to a custom user.

--- ReNaChatBackend/users/models.py
+++ ReNaChatBackend/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(blank=True, max_length=255)
+    bio = models.CharField(max_length=300, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', null=True)
+    def __str__(self):
+        return self.email
