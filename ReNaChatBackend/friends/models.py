72cce4fb7289b214fcf7d813ec86734ee628fa96
Aaron Vogler <Aaron.Vogler@Colorado.EDU>
2020-01-15 20:05:05 -0700

    Set Friend model to import CustomUser.

--- ReNaChatBackend/friends/models.py
+++ ReNaChatBackend/friends/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
 class Friend(models.Model):
--- ReNaChatBackend/friends/views.py
+++ ReNaChatBackend/friends/views.py
@@ -1,3 +1,123 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def requestFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[requestFriend] - User <', serializer.data['username'], '> is trying to make a friend request to ', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.add_pending_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def removeRequestedFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[requestFriend] - User <', serializer.data['username'], '> is trying to make a friend request to ', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.remove_requested_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def removePendingFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[requestFriend] - User <', serializer.data['username'], '> is trying to make a friend request to ', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.remove_pending_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def confirmFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[confirmFriend] - User <', serializer.data['username'], '> is trying to make approve a friend request to ', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.confirm_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def removeFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[requestFriend] - User <', serializer.data['username'], '> is trying to remove friend:', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.remove_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['post'])
+@permission_classes([IsAuthenticated])
+def removePendingFriend(request):
+    form = CustomUserDetailsForm(request.POST)
+    serializer = UserSerializer(request.user, many=False)
+    print('[requestFriend] - User <', serializer.data['username'], '> is trying to make a friend request to ', request.POST.get('username', ''))
+
+    if form.is_valid():
+        new_friend = CustomUser.objects.get(username=form.cleaned_data['username'])
+        Friend.remove_pending_friend(request.user, new_friend)
+
+        return Response({"success": "true", "current_user": serializer.data['username']})
+
+    print("Form errors: ", form.errors)
+    return HttpResponse(status=500)
+
+@api_view(['get'])
+@permission_classes([IsAuthenticated])
+def getPendingFriends(request):
+    friendObj = Friend.objects.get(user_profile_id=request.user.id)
+    pendingFriendUserList = friendObj.pending_friends.all()
+    serializer = UserSerializer(pendingFriendUserList, many=True)
+    return Response(serializer.data)
+
+@api_view(['get'])
+@permission_classes([IsAuthenticated])
+def getRequestedFriends(request):
+    friendObj = Friend.objects.get(user_profile_id=request.user.id)
+    pendingFriendUserList = friendObj.requested_friends.all()
+    serializer = UserSerializer(pendingFriendUserList, many=True)
+    return Response(serializer.data)
+
+@api_view(['get'])
+@permission_classes([IsAuthenticated])
+def getFriends(request):
+    friendObj = Friend.objects.get(user_profile_id=request.user.id)
+    friendUserList = friendObj.friends.all()
+    serializer = UserSerializer(friendUserList, many=True)
+    return Response(serializer.data)
