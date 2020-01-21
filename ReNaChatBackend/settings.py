4bf40b6c86a324933f235033e0429e9d8aeba913
Aaron Vogler <Aaron.Vogler@Colorado.EDU>
2020-01-20 21:01:54 -0700

    Fine tune deployment settings for heroku.

--- ReNaChatBackend/settings.py
+++ ReNaChatBackend/settings.py
@@ -122,8 +122,12 @@ WSGI_APPLICATION = 'ReNaChatBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
